c9cff6fdd2425350dfeab89929c2f3c26b34e318
mojiehua <1612730560@qq.com>
2017-07-26 12:10:55 +0800

    支持用户自定义 Transform 处理函数

--- src/transform/__init__.py
+++ src/transform/__init__.py
@@ -3,6 +3,8 @@
 #
 # Author: Alex
 # Created Time: 2017年05月16日 星期二 10时58分39秒
+
+import imp
 from copy import deepcopy
 from importlib import import_module
 
@@ -11,15 +13,18 @@ class Transform:
     config = {}
 
     def __init__(self, config):
-        for row in config:
-            transform_name = row['type'] + '.' + row['name']
-            module = import_module('src.transform.' + transform_name)
-            row['_transform'] = module.Transform(row)
+        for each_config in config:
+            if 'user' == each_config['type']:
+                module = imp.load_source('Transform', config['path'])
+                each_config['transformFunc'] = getattr(module, config['func'])
+            else:
+                module = import_module('src.transform.' + each_config['type'] + '.' + each_config['name'])
+                each_config['transformFunc'] = module.Transform(each_config).do
 
-            if 'log' not in row:
-                row['log'] = False
-            elif type(row['log']) != bool:
-                raise Exception("log type is not bool in %s" % transform_name)
+            if 'log' not in each_config:
+                each_config['log'] = False
+            elif type(each_config['log']) != bool:
+                raise Exception("log type is not bool in ",each_config)
 
         self.config = config
 
@@ -30,7 +35,7 @@ class Transform:
             if conf['log']:
                 _rows = deepcopy(rows)   # 保存原有的数据
 
-            rows = conf['_transform'].do(rows)
+            rows = conf['transformFunc'](rows)
             if conf['log'] and rows != _rows:
                 print("%s: %s =>" % (conf['type'], conf['name']))
                 print(rows)
